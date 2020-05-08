0e60a479723310b2ddb8c553e8ddd0bfbf0e6530
Hide <hshidara@ucdavis.edu>
2020-05-07 17:17:12 -0700

    Updates

---
+++ precache-manifest.35607fe9eefdeb4d8e153b9a20f4118a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8552d3859aea045cfd4314796d0a394b",
+    "url": "/leadgen/index.html"
+  },
+  {
+    "revision": "64b243067a0179685444",
+    "url": "/leadgen/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bce03a008e5606d98412",
+    "url": "/leadgen/static/js/2.12679a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leadgen/static/js/2.12679a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b243067a0179685444",
+    "url": "/leadgen/static/js/main.86872db4.chunk.js"
+  },
+  {
+    "revision": "2e8166e9e47f5c6d61ed",
+    "url": "/leadgen/static/js/runtime-main.6bf67275.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leadgen/static/media/logo.5d5d9eef.svg"
+  }
+]);
